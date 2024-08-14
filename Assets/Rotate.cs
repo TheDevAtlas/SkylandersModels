c6c85d1fc8d6b609e83f6f9fda57bba1301fb578
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2024-08-14 17:10:07 -0400

    Init

---
+++ Assets/Rotate.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, speed, 0) * Time.deltaTime);
+    }
+}
